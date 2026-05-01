5451a3c661b6d22f9f53f54d3bba26735bfdc4e9
siwar777 <mejrisw34@gmail.com>
2026-05-01 13:08:49 +0100

    fix CORS and change port to 3001

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  await app.listen(3001);
 }
 bootstrap();
